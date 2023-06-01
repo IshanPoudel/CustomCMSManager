6ec949e01c853e15ea245fc44974d7307bffce95
IshanPoudel <ishan.poudel@mavs.uta.edu>
2023-06-01 12:25:31 -0500

    Added ability to connect to database , refactored code

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
+// This assumes the user is already logged in. Once prototyped , add functionality for logging in.
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const fs = require("fs");
+const mysql = require('mysql');
 
 
 
 
 
 
+let connection=''
+// Connect to mysql
 
 // Reading from a file is the same as fetching from an api , you need to handle both cases. 
 
@@ -18,16 +23,25 @@ const startServer = async()=>
         // Try reading in the data from the config file. 
         const jsonData = await fs.promises.readFile('server_config.json' , {encoding:'utf-8'} );
         // If succesfully read. 
-        console.log(jsonData)
+        
         
         // Start the server. 
         port = JSON.parse(jsonData).port
+
 
 
         app.listen(port , ()=>
         {
             console.log(`Server is running on Port ${port}`);
         })
+
+        // Initiate mysql connection. 
+
+        await connectToSql();
+
+        
+        
+
 
     }
     catch(err)
@@ -38,10 +52,73 @@ const startServer = async()=>
     }
 }
 
-
-startServer();
-// At this point , our app should be listening. 
-app.get("/" , (req,res)=>
+const connectToSql = async()=>
 {
-    res.send("Hello world");
-})
+    try
+    {
+        // Try reading in the data from the config file. 
+        const jsonData = await fs.promises.readFile('api_keys.json' , {encoding:'utf-8'} );
+        // If succesfully read. 
+        
+        // Start the server. 
+        masterUser = JSON.parse(jsonData).master_user
+
+        
+
+        db_endpoint = masterUser.db_endpoint
+        username = masterUser.username
+        password_user = masterUser.password
+        port_num = masterUser.port
+
+         // Initiate mysql connection. 
+
+        connection = mysql.createConnection({
+            host: db_endpoint , 
+            user : username , 
+            password: password_user ,
+            port: port_num
+             
+            
+        });
+
+        // console.log(db_endpoint)
+
+        connection.connect((error)=>
+        {
+            if (error)
+            {
+                console.error("Error connecting to the database" + error.stack)
+                return;
+            }
+
+            console.log('Connected to the master_database ');
+        })
+    
+
+    }
+    catch(err)
+    {
+        console.log(err)
+        console.log("COuld not connect to database. ")
+
+    }
+}
+
+
+startServer().then(()=>
+{
+  app.get("/" , (rew , res)=>
+  {
+    res.send('Hello World');
+  })
+
+});
+
+// At this point , our app should be listening. 
+
+
+
+
+
+
+
