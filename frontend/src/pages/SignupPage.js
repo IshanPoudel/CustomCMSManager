e23e4f4db180b6a09bfb8fee6ef7d4d511f5fcd3
IshanPoudel <ishan.poudel@mavs.uta.edu>
2023-06-12 20:52:02 -0500

    Succesfully linked signup to backend

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer , toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//API to call the backend
+const url ='http://localhost:8000/add_new_user';
 
 const SignupPage = () => {
   const [username, setUsername] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
+  const [successMsg , setSuccess] = useState('');
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -19,13 +27,60 @@ const SignupPage = () => {
     setEmail(e.target.value);
   };
 
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (username === '' || name === '' || email === '') {
+    if (username === '' || name === '' || email === '' || password === '' || confirmPassword === '') {
       setError('Please fill in all the fields.');
+    } else if (password !== confirmPassword) {
+      setError('Passwords do not match.');
     } else {
       // Add your sign-up logic here
+
+      const data = {
+        username: username , 
+        email : email ,
+        password: password
+      }
+
+      //Fetch and call the api
+      fetch(url , 
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+          .then(response=>response.json())
+          .then(data=>{
+            console.log('Response' , data);
+            setSuccess(data);
+            toast.success( 'Account created succesfully!' , {
+              autoClose: 5000 , 
+              onOpen: ()=>{
+                setTimeout(()=>
+                {
+                  window.location.href = '/login';
+                } , 5000);
+              }
+            })
+
+          })
+          .catch(error=>
+            {
+                console.log('Error' , error)
+            });
+    
+    
     }
   };
 
@@ -74,6 +129,32 @@ const SignupPage = () => {
               onChange={handleEmailChange}
             />
           </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="confirmPassword"
+              type="password"
+              placeholder="Confirm your password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+            />
+          </div>
           <div className="flex items-center justify-center">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -89,6 +170,7 @@ const SignupPage = () => {
             </Link>
           </p>
         </form>
+        <ToastContainer/>
       </div>
     </div>
   );
